d7cf3bcc7adedf15d8a047e69712e04ca92284b8
Benjamin Winter - Leinweber <Ben@Benjamins-MacBook-Pro.local>
2022-06-27 22:47:35 +0100

    static_url now using project dir

--- giftcard/settings.py
+++ giftcard/settings.py
@@ -134,7 +134,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = 'static/live4yoga_app/'
+STATIC_URL = 'static/giftcard_app/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
