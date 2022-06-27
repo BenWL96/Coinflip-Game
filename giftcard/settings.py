257d9080a06d8809c3eccd99b3851b75d55d7be5
Benjamin Winter - Leinweber <Ben@Benjamins-MacBook-Pro.local>
2022-06-27 22:48:49 +0100

    take 2 of previous commit with correct location

--- giftcard/settings.py
+++ giftcard/settings.py
@@ -134,7 +134,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = 'static/giftcard_app/'
+STATIC_URL = 'static/giftcard_application/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
